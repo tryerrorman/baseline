45ac39b716dd65114fbd703413229df0c6d02207
tryerrorman <fengleilwang@gmail.com>
2017-11-03 12:44:34 +0800

    new model

---
+++ model.py
@@ -0,0 +1,22 @@
+import os
+import torch
+import torch.nn as nn
+
+class bkpmodel(object):
+	"""docstring for bkpmodel"""
+	def __init__(self, arg):
+		super(bkpmodel, self).__init__()
+		self.arg = arg
+		self.conv1 = nn.Conv2d
+
+	
+	def forward(self):
+		pass
+	def save_network():
+		pass
+
+	def load_network():
+		pass
+
+	def update_learning_rate():
+		pass
